a5d539115428fbb0222e11e7d38306d35dd36db8
David Molinari <Dmolina5@Davids-MBP-2.hsd1.pa.comcast.net>
2020-04-13 20:51:30 -0400

    Updated sprint

---
+++ data/helpers/actionsRouter.js
@@ -0,0 +1,67 @@
+const express = require("express");
+
+const actionHelpers = require("./actionModel")
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    let object = {
+            project_id: req.params.id,
+            description: req.body.description,
+            notes: req.body.notes
+        }
+    actionHelpers.insert(object)
+        .then(actions => {
+            console.log(actions)
+            res.status(200).json(actions);
+        })
+        .catch(error => {
+        // log error to server
+            console.log(error);
+            res.status(500).json({
+                message: 'Error posting the resource',
+        });
+    });
+
+})
+
+router.put('/', (req, res) => {
+    let object = {
+        project_id: req.params.id,
+        description: req.body.description,
+        notes: req.body.notes
+    } 
+    actionHelpers.update(req.params.id, object)
+        .then(actions => {
+            console.log(actions)
+            res.status(200).json(actions);
+        })
+        .catch(error => {
+        // log error to server
+            console.log(error);
+            res.status(500).json({
+                message: 'Error putting the resource',
+        });
+    });
+})
+
+router.delete('/', (req, res) => {
+    actionHelpers.remove(req.params.id)
+        .then(count => {
+        if (count > 0) {
+            res.status(200).json({ message: 'The resource has been nuked' });
+        } else {
+            res.status(404).json({ message: 'The resource could not be found' });
+        }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the resource',
+      });
+    });
+  });
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -13,68 +13,85 @@ I need this code, but don't know where, perhaps should make some middleware, don
 Go code!
 */
 const express = require('express');
-const actionHelpers = require("./data/helpers/actionModel");
-const projectHelpers = require("./data/helpers/projectModel");
+
+const projectsRouter = require('./data/helpers/projectsRouter')
 
 const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+server.use('/api/projects', projectsRouter);
+
+server.listen(4000, () => {
+    console.log('\n* Server Running on http://localhost:4000 *\n');
   });
 
-  server.get('/projects/:id', (req, res) => {
-    let id = req.params.id;
-    res.send(projectHelpers.get(id));
-});
+// const actionHelpers = require("./data/helpers/actionModel");
+// const projectHelpers = require("./data/helpers/projectModel");
 
-server.post('/projects', (req, res) => {
-    let object = {
-            name: req.body.name,
-            description: req.body.description
-        }
-    res.send(projectHelpers.insert(object));
-})
+// const server = express();
+// server.use(express.json());
 
-server.put('/projects/:id', (req, res) => {
-    let object = {
-        name: req.body.name,
-        description: req.body.description
-    } 
-    res.send(projectHelpers.update(req.params.id, object))
-})
+// server.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
 
-server.delete('/projects/:id', (req, res) => {
-    res.send(projectHelpers.remove(req.params.id))
-})
+// server.get('/projects/:id', (req, res) => {
+//     let id = req.params.id;
+//     res.send(projectHelpers.get(id));
+// });
 
-server.get('/actions/:id', (req, res) => {
-    let id = req.params.id;
-    res.send(actionHelpers.get(id));
-});
+// server.post('/projects', (req, res) => {
+//     let object = {
+//             name: req.body.name,
+//             description: req.body.description
+//         }
+//     res.send(projectHelpers.insert(object))
+//         // .then() <- may need to send updated database
+// })
 
-server.post('/actions', (req, res) => {
-    let object = {
-            name: req.body.name,
-            description: req.body.description
-        }
-    res.send(actionHelpers.insert(object));
-})
+// server.put('/projects/:id', (req, res) => {
+//     let object = {
+//         name: req.body.name,
+//         description: req.body.description
+//     } 
+//     res.send(projectHelpers.update(req.params.id, object))
+// })
 
-server.put('/action/:id', (req, res) => {
-    let object = {
-        name: req.body.name,
-        description: req.body.description
-    } 
-    res.send(actionHelpers.update(req.params.id, object))
-})
+// server.delete('/projects/:id', (req, res) => {
+//     res.send(projectHelpers.remove(req.params.id))
+// })
 
-server.delete('/actions/:id', (req, res) => {
-    res.send(actionHelpers.remove(req.params.id))
-})
+// server.get('/actions/:id', (req, res) => {
+//     let id = req.params.id;
+//     res.send(actionHelpers.get(id));
+// });
 
-server.get('/projects/:id/actions', (req, res) => {
-    res.send(projectHelpers.getProjectActions(req.params.id))
-})
+// server.post('/actions', (req, res) => {
+//     let object = {
+//             name: req.body.name,
+//             description: req.body.description
+//         }
+//     res.send(actionHelpers.insert(object));
+// })
 
-server.listen(8000, () => console.log('API running on port 8000'));
+// server.put('/action/:id', (req, res) => {
+//     let object = {
+//         name: req.body.name,
+//         description: req.body.description
+//     } 
+//     res.send(actionHelpers.update(req.params.id, object))
+// })
+
+// server.delete('/actions/:id', (req, res) => {
+//     res.send(actionHelpers.remove(req.params.id))
+// })
+
+// server.get('/projects/:id/actions', (req, res) => {
+//     res.send(projectHelpers.getProjectActions(req.params.id))
+// })
+
+// server.listen(8000, () => console.log('API running on port 8000'));
